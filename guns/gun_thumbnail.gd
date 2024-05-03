ccc8855b145657cf776c521afed9d8be86b095d5
jdbecker <jdbecker20@gmail.com>
2024-05-03 18:47:40 -0500

    put tooltip on root so it's always on-top

--- guns/gun_thumbnail.gd
+++ guns/gun_thumbnail.gd
@@ -4,9 +4,11 @@ extends PanelContainer
 @export var gun_stats: GunStats : set = _set_gun_stats
 
 const GUN = preload("res://guns/gun.tscn")
+const TOOLTIP = preload("res://guns/tooltip.tscn")
+
+var _tooltip: Tooltip
 
 @onready var sub_viewport: SubViewport = $SubViewportContainer/SubViewport as SubViewport
-@onready var tooltip: Tooltip = $Tooltip as Tooltip
 
 
 func _set_gun_stats(value: GunStats) -> void:
@@ -15,16 +17,17 @@ func _set_gun_stats(value: GunStats) -> void:
 	if not is_node_ready():
 		await ready
 	
-	tooltip.gun_stats = gun_stats
 	var gun := GUN.instantiate() as Gun
 	gun.stats = gun_stats
 	sub_viewport.add_child(gun)
 
 
 func _on_area_2d_mouse_entered() -> void:
-	tooltip.show()
-	tooltip.position_over_mouse.call_deferred()
+	_tooltip = TOOLTIP.instantiate()
+	_tooltip.gun_stats = gun_stats
+	_tooltip.position_over_mouse.call_deferred()
+	get_tree().root.add_child(_tooltip)
 
 
 func _on_area_2d_mouse_exited() -> void:
-	tooltip.hide()
+	_tooltip.queue_free()
