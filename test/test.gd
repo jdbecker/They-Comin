c48ff87e81b821a21d4769426a15afe7300783b1
jdbecker <jdbecker20@gmail.com>
2024-05-05 00:12:17 -0500

    fake some tests

--- test/test.gd
+++ test/test.gd
@@ -1,5 +1,7 @@
-extends SceneTree
+extends MainLoop
 
 func _init() -> void:
 	print("Hello world!")
-	quit()
+	assert(true, "Passed a test")
+	assert(false, "Failed a test")
+	print("Done running tests")
