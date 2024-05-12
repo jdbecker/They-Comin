93b2882e0d4281f21c14d3a07d9076d008639dd3
Robert Whiting <rswhiting@gmail.com>
2024-05-11 18:03:51 -0600

    add a walther p88, lamp, and chairs

--- controllers/scripts/player.gd
+++ controllers/scripts/player.gd
@@ -22,6 +22,7 @@ signal cheat_queue_wave
 @onready var kill_count_label: Label = %KillCount as Label
 @onready var gun: Gun = $Camera3D/HandSlot/Gun
 @onready var inventory: Inventory = %Inventory
+@onready var floor_lamp = $CollisionShape3D/floor_lamp
 
 const TILT_LOWER_LIMIT := deg_to_rad(-90.0)
 const TILT_UPPER_LIMIT := deg_to_rad(90.0)
@@ -51,6 +52,8 @@ func _ready() -> void:
 	if not is_multiplayer_authority():
 		ready.connect(func() -> void: broadcast_gun_stats.rpc_id(name.to_int()))
 		return
+	
+	floor_lamp.hide()
 	
 	# Get mouse input
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
