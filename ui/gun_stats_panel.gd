972cdc6dc687f6ffc4269169bd4350888d6695ae
jdbecker <jdbecker20@gmail.com>
2024-05-11 22:50:07 -0500

    clean up some static warnings

--- ui/gun_stats_panel.gd
+++ ui/gun_stats_panel.gd
@@ -7,9 +7,9 @@ extends PanelContainer
 @onready var damage: Label = %Damage
 @onready var rate_of_fire: Label = %RateOfFire
 
-var DEFAULT = Color(1, 1, 1)
-var RED = Color(1, 0, 0)
-var GREEN = Color(0, 1, 0)
+const DEFAULT := Color(1, 1, 1)
+const RED := Color(1, 0, 0)
+const GREEN := Color(0, 1, 0)
 
 func _set_gun_stats(value: GunStats) -> void:
 	gun_stats = value
@@ -18,10 +18,10 @@ func _set_gun_stats(value: GunStats) -> void:
 		await ready
 	
 	gun_type.text = gun_stats.type_name()
-	_set_stylized_gun_stat("damage", damage, gun_stats.damage, Global.data.gun_in_hand.damage)
-	_set_stylized_gun_stat("rate", rate_of_fire, gun_stats.rate_of_fire, Global.data.gun_in_hand.rate_of_fire)
+	_set_stylized_gun_stat(damage, gun_stats.damage, Global.data.gun_in_hand.damage)
+	_set_stylized_gun_stat(rate_of_fire, gun_stats.rate_of_fire, Global.data.gun_in_hand.rate_of_fire)
 
-func _set_stylized_gun_stat(stat_type: String, element: Label, stat: float, in_hand: float) -> void:
+func _set_stylized_gun_stat(element: Label, stat: float, in_hand: float) -> void:
 	element.text = str(stat)
 	if in_hand > stat:
 		element.add_theme_color_override("font_color", RED)
